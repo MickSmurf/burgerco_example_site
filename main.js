0ba069c84c94c5df8d20dad362101d1ebceb48ab
Mike Berg <mike@mikeberg.de>
2022-05-12 09:28:53 +0200

    scroll to top and smooth scrolling add

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 const btn = document.getElementById('menu-btn');
 const nav = document.getElementById('menu');
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-to-top");
+
+const scrollContainer =() => {
+  return document.documentElement || document.body;
+}
+
+const goToTop = () => {
+  document.body.scrollIntoView({
+    behavior: "smooth",
+  });
+}
+
+
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden")
+  } else {
+    backToTopButton.classList.add("hidden")
+  }
+});
+
+backToTopButton.addEventListener("click", goToTop);
 
 function navToggle() {
   btn.classList.toggle('open')
@@ -8,4 +31,6 @@ function navToggle() {
 }
 
 btn.addEventListener('click', navToggle);
+
+
 
